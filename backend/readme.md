fabbc6aa4f43eec4471af07c9a073f0ec0052476
Gayatri P <gayatri.potawad@softtrust.ai>
2025-09-09 22:21:39 +0100

    sql connection changes

--- backend/readme.md
+++ backend/readme.md
@@ -5,5 +5,24 @@ sls deploy
 
 sls offline start
 
+
 python3 -m uvicorn main:app --reload
 
+
+pip install -r requirements.txt --target ./package
+cp -r src main.py package/
+cd package
+zip -r ../lambda.zip .
+
+
+
+
+docker run -it -v "$PWD":/app public.ecr.aws/lambda/python:3.11 bash
+cd /app/backend
+mkdir build && cd build
+pip install -r ../requirements.txt -t .
+
+cp ../main.py .
+cp -r ../src .
+
+zip -r9 ../lambda.zip .
--- backend/src/db/connection.py
+++ backend/src/db/connection.py
@@ -1,8 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://test:test@localhost:5432/filmx"
+DB_USER = os.getenv("DB_USER", "test")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "test")
+DB_HOST = os.getenv("DB_HOST", "localhost")   # will be RDS/Aurora host in AWS
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "filmx")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
